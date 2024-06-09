5c717911525ab6b53211bb3a51bec440f522b043
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-06-09 14:11:01 +0200

    Updated files

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-
+import Vans from "./pages/Vans.jsx";
+import "./Server.js";
 function App() {
   return (
     <BrowserRouter>
@@ -18,6 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
