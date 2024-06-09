607ec135f79ecc265b63a33b4c3eeeee9c14891e
Mandla Dyonase <156649157+Mandla7784@users.noreply.github.com>
2024-06-09 18:20:14 +0200

    Add files via upload

--- api.js
+++ api.js
@@ -1,72 +1,49 @@
-import { initializeApp } from "firebase/app";
 
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  getDoc,
-  doc,
-} from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCnDpR9Zs5wS6jwN9h2Vz8yM5ZV2Hg9D4A",
-  authDomain: "wfc-2401-group-b.firebaseapp.com",
-  projectId: "wfc-2401-group-b",
-  storageBucket: "wfc-2401-group-b.appspot.com",
-  messagingSenderId: "108668040",
-  appId: "1:108668040:web:5b7b7a9a7a5b7b9b9a5b9d",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-const VansCollectionRef = collection(db, "vans");
-
-export async function getVans() {
-  const snapShot = await getDocs(VansCollectionRef);
-  const vans = snapShot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-  return vans;
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(() => resolve(), ms))
 }
 
-export async function getVan(id) {
-  const docRef = doc(db, "vans", id);
-  const snapShot = await getDoc(docRef);
-
-  return {
-    ...snapShot.data(),
-    id: snapShot.id,
-  };
+export async function getVans(id) {
+    const url = id ? `/api/vans/${id}` : "/api/vans"
+    const res = await fetch(url)
+    if (!res.ok) {
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json()
+    return data.vans
 }
 
 export async function getHostVans(id) {
-  const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw {
-      message: "Failed to fetch vans",
-      statusText: res.statusText,
-      status: res.status,
-    };
-  }
-  const data = await res.json();
-  return data.vans;
+    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
+    const res = await fetch(url)
+    if (!res.ok) {
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json()
+    return data.vans
 }
 
-export async function LoginUser(creds) {
-  const res = await fetch("/api/user/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(creds),
-  });
-  if (!res.ok) {
-    throw {
-      message: "Failed to login",
-      statusText: res.statusText,
-      status: res.status,
-    };
-  }
-  const data = await res.json();
-  return data.user;
+export async function loginUser(creds) {
+    const res = await fetch("/api/login",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+
+    return data
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Vans from "./pages/vans/Vans.jsx";
-import VanDetail from "./pages/vans/VansDetails.jsx";
-import "./Server.js";
+/**
+ * Challenge:
+ * Bootstrap the VanLife project by creating the first 2 routes:
+ * Home and About.
+ * 
+ * Also include the navbar that can link between the two routes.
+ * For now, you'll either need to copy/paste the navbar code
+ * to both Home and About pages, or you'll need to find a place
+ * to put it where it can be shared between the two pages.
+ * (Don't overthink this part - just do whatever is easiest for
+ * you because we'll learn a better approach very soon)
+ * 
+ * Review challenge: do all the CSS yourself based on the design
+ * linked in the slides.
+ */
+
 function App() {
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <h1>Start here</h1>
+  )
 }
 
-export default App;
+export default App
