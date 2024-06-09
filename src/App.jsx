3991040ac301a1af5ef05f96c4cf0c1d01c15b23
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-06-09 17:11:48 +0200

    Added a Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Vans from "./pages/Vans.jsx";
-import VanDetail from "./pages/VansDetails.jsx";
+import Vans from "./pages/vans/Vans.jsx";
+import VanDetail from "./pages/vans/VansDetails.jsx";
 import "./Server.js";
 function App() {
   return (
     <BrowserRouter>
-      <header className="site-header">
-        <Link className="site-logo" to="/">
-          #VanLife
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
