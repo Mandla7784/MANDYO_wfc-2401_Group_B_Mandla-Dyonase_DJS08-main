bbee6e4e3d0b7b9c0323f974a1be0a9d794bb0e4
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-06-09 16:33:24 +0200

    Added firebase

---
+++ api.js
@@ -0,0 +1,35 @@
+import { initializeApp } from "firebase/app";
+
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCnDpR9Zs5wS6jwN9h2Vz8yM5ZV2Hg9D4A",
+  authDomain: "wfc-2401-group-b.firebaseapp.com",
+  projectId: "wfc-2401-group-b",
+  storageBucket: "wfc-2401-group-b.appspot.com",
+  messagingSenderId: "108668040",
+  appId: "1:108668040:web:5b7b7a9a7a5b7b9b9a5b9d",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const VansCollectionRef = collection(db, "vans");
+
+export async function getVans() {
+  const snapShot = await getDocs(VansCollectionRef);
+  const vans = snapShot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  return vans;
+}
+
+export async function getVan(id) {
+  const docRef = doc(db, "vans", id);
+  const snapShot = await getDoc();
+}
